84b4369074cbebc2338ce63d07483eb810f00d32
Nikola Nakovski <117409706+nnakovskidesigner@users.noreply.github.com>
2025-03-18 14:29:52 +0100

    .
    .

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-    return (
-        <main className="flex flex-col items-center justify-center min-h-screen text-center">
-            <h1 className="text-4xl font-bold">Hello World</h1>
-            <p className="text-lg mt-4">Powered by Telekom</p>
-        </main>
-    );
-}
